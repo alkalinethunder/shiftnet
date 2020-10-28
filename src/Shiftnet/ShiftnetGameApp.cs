a31e617b9883f13c0688ed84037d5239c63dc6e4
Michael VanOverbeek <theplexgate@gmail.com>
2020-10-27 22:56:50 -0400

    Start porting the main menu.

--- src/Shiftnet/ShiftnetGameApp.cs
+++ src/Shiftnet/ShiftnetGameApp.cs
@@ -1,9 +1,16 @@
 ﻿using AlkalineThunder.Pandemic;
+using AlkalineThunder.Pandemic.Scenes;
 
 namespace Shiftnet
 {
     public class ShiftnetGameApp : App
     {
+        private SceneSystem SceneSystem
+            => GetModule<SceneSystem>();
         
+        protected override void OnLoad()
+        {
+            SceneSystem.GoToScene<MainMenu>();
+        }
     }
 }
