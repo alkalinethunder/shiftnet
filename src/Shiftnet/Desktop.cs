93d959b7db246e77f85f4ed2d666a6866515ef6e
Michael VanOverbeek <theplexgate@gmail.com>
2020-10-31 22:49:25 -0400

    Make Shiftnet run cmd.exe in the terminal because why not?

--- src/Shiftnet/Desktop.cs
+++ src/Shiftnet/Desktop.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using AlkalineThunder.Pandemic.Gui.Controls;
 using AlkalineThunder.Pandemic.Gui.Markup;
 using AlkalineThunder.Pandemic.Input;
@@ -23,6 +26,13 @@ namespace Shiftnet
         private TextBlock _time = null;
         private Button _appLauncherOpen = null;
         private Button _settingsOpen = null;
+        private ConsoleControl _console;
+        private Process _proc;
+        private Task _outTask;
+        private Task _inTask;
+        private object _inLock = new object();
+        private object _outLock = new object();
+            
         
         protected override void OnLoad()
         {
@@ -33,10 +43,26 @@ namespace Shiftnet
             _settingsOpen = Gui.FindById<Button>("settings");
             
             _settingsOpen.Click += SettingsOpenOnClick;
+
+            _console = Gui.FindById<ConsoleControl>("console");
+            StartConceptShell();
             
             base.OnLoad();
         }
 
+        private void StartConceptShell()
+        {
+            _proc = new Process();
+            _proc.StartInfo.FileName = "cmd.exe";
+            _proc.StartInfo.RedirectStandardOutput = true;
+            _proc.EnableRaisingEvents = true;
+            _proc.StartInfo.RedirectStandardInput = true;
+            _proc.Start();
+
+            _outTask = ReadOutput();
+            _inTask = SendInput();
+        }
+        
         private void SettingsOpenOnClick(object? sender, MouseButtonEventArgs e)
         {
             if (_settingsWindow == null)
@@ -54,8 +80,38 @@ namespace Shiftnet
         protected override void OnUpdate(GameTime gameTime)
         {
             _time.Text = DateTime.Now.ToShortTimeString();
+
             base.OnUpdate(gameTime);
         }
-    
+
+
+        private async Task ReadOutput()
+        {
+            var block = new char[1024];
+            
+            while (true)
+            {
+                var bytesRead = await _proc.StandardOutput.ReadAsync(block, 0, block.Length);
+
+                await App.GameLoop.Invoke(() =>
+                {
+                    for (var i = 0; i < bytesRead; i++)
+                        _console.Write(block[i].ToString());
+                });
+
+            }
+        }
+
+        private async Task SendInput()
+        {
+            while (true)
+            {
+                var text = await _console.Input.ReadLineAsync();
+                await App.GameLoop.Invoke(() =>
+                {
+                    _proc.StandardInput.WriteLine(text);
+                });
+            }
+        }
     }
 }
